e7ee259aec2d1cf06acbe56c1d78e570aa9edb9a
sherecks <joaoppn00@gmail.com>
2026-06-17 15:18:44 -0300

    Add new things

--- src/catalog.js
+++ src/catalog.js
@@ -1,12 +1,6 @@
 // Add new models here — just put the .stl in public/models/ and add an entry.
 export const CATALOG = [
   {
-    id: 'topwater-lure',
-    name: 'Top Water Lure',
-    description: 'Isca de pesca',
-    url: '/models/TopWaterLure.stl',
-  },
-  {
     id: 'fishingpopper',
     name: 'Fishing Popper',
     description: 'Isca de pesca',
@@ -17,5 +11,23 @@ export const CATALOG = [
     name: 'Magik Stick Half',
     description: 'Isca de pesca',
     url: '/models/MagicStickHalf.stl',
+  },
+  {
+    id: 'planter',
+    name: 'Large Planter',
+    description: 'Vaso',
+    url: '/models/LargePlanter.3mf',
+  },
+  {
+    id: 'Saucer',
+    name: 'Saucer Planter',
+    description: 'Prato',
+    url: '/models/SaucerPlanter.3mf',
+  },
+  {
+    id: 'glasses',
+    name: 'Eyeglass Holder',
+    description: 'Suporte',
+    url: '/models/EyeglassHolder.stl',
   },
 ]
